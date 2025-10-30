d1d297380a5ac188f0be1d2061fd7a8b151136a2
VilheIms <boysepticeye@gmail.com>
2025-10-30 14:39:24 +0200

    Tagard programma var izveidot personu, tomēr vēl nevar izdzēst

--- src/GalvenaisTels.java
+++ src/GalvenaisTels.java
@@ -1,15 +1,88 @@
+import java.io.Serializable;
+import java.util.regex.Pattern;
 
-public class GalvenaisTels {
+import javax.swing.JOptionPane;
+
+public class GalvenaisTels implements Serializable, Comparable<GalvenaisTels>{
 	
 	//Attribūti
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9222023328378505339L;
 	public static int IzpildMis=0;
-	
 	
 	public GalvenaisTels(int IzpildMis) {
 		GalvenaisTels.IzpildMis=IzpildMis;
 		
 		
+	}
+
+	//Attribūti
+	
+		private String masinas;
+		private int ieroci = 1;
+		private double nauda;
+		
+		public GalvenaisTels() { };
+		
+		public String getMasinas() {
+			return masinas;
+		}
+		public double getNauda() {
+			return nauda;
+		}
+		public int getIeroci() {
+			return ieroci;
+		}
+		public void setMasina(String masinas) {
+			this.masinas = masinas;
+		}
+		public void setNauda(double nauda) {
+			this.nauda= nauda;
+		}
+		public void setIeroci(int ieroci) {
+			this.ieroci = ieroci;
+		}
+			public static String virknesParbaude(String zinojums, String noklusejums) {
+				String virkne;
+				do {
+					virkne = JOptionPane.showInputDialog(zinojums, noklusejums);
+					if(virkne == null)
+						return null;
+					virkne = virkne.trim();
+				}while(!Pattern.matches("^[\\p{L} ]+$", virkne));
+				return virkne;
+			}
+			public static String ievade;
+			
+			
+			public static double skaitlaParbaude(String zinojums, double min, double max) {
+				
+				double skaitlis;
+				while(true) {
+					ievade = JOptionPane.showInputDialog(null, zinojums, "Datu ievade", JOptionPane.INFORMATION_MESSAGE);
+					if(ievade == null)
+						return -1;
+					try {
+						skaitlis = Double.parseDouble(ievade);
+						if(skaitlis<min || skaitlis>max) {
+							JOptionPane.showMessageDialog(null, "Norādīts nederīgs intervāls", "Nekorekti dati", JOptionPane.WARNING_MESSAGE);
+							continue;
+						}
+						return skaitlis;
+					}catch(NumberFormatException e) {
+						JOptionPane.showMessageDialog(null, "Netika ievadīts vesels skaitlis", "Nekorekti dati", JOptionPane.ERROR_MESSAGE);
+					}
+				}
+			}
+	
+	
+	
+	@Override
+	public int compareTo(GalvenaisTels s) {
+		return this.getMasinas().compareTo(s.getMasinas());
 	}
 	
 	
--- src/GrandTheftAutoV.java
+++ src/GrandTheftAutoV.java
@@ -1,12 +1,74 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.regex.Pattern;
+
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
 
 public class GrandTheftAutoV {
-	
-	public static void main(String[] args) {
-		
-		System.out.println(GalvenaisTels.IzpildMis);
-		Misijas.Misija();
-		System.out.println(GalvenaisTels.IzpildMis);
-	}
-}
 
-	
+	public static void main(String[] args) {
+		String izvele;
+		int izvelesID;
+		String[] darbibas = {"Izvēlies personu", "Personu saraksts", "Dzēst personu", "Izsaukt metodi", "Aizvērt programmu"};
+		String[] veidi = {"Frenklins", "Trevors", "Maikls"};
+		String[] atbilde = {"Jā", "Nē"};
+		ArrayList<Object> personas = new ArrayList<>();
+		GalvenaisTels persona = null;
+		do {
+			izvele = (String)JOptionPane.showInputDialog(null, "Izvēlies darbību", "Izvēlne", JOptionPane.QUESTION_MESSAGE, null, darbibas, darbibas[0]);
+			if(izvele == null)
+				break;
+			izvelesID = Arrays.asList(darbibas).indexOf(izvele);
+			switch(izvelesID) {
+			case 0:
+				izvele = (String)JOptionPane.showInputDialog(null, "Kuru personu gribi izvēlēties?", "Izvēlne", JOptionPane.QUESTION_MESSAGE, null, veidi, veidi[0]);
+				if(izvele == null)
+					break;
+				izvelesID = Arrays.asList(veidi).indexOf(izvele);
+				
+				String masina = GalvenaisTels.virknesParbaude("Ievadi mašīnas nosaukumu", "Bravado Buffalo");
+				
+				double nauda = GalvenaisTels.skaitlaParbaude("Ievadi sākuma naudu (no 1000-10000)", 1000.00, 10000.00);
+				
+				int ieroci = (int) GalvenaisTels.skaitlaParbaude("Cik būs ieroči?", 1, 59);
+				
+				
+				if(izvelesID == 0) {
+					persona = new GalvenaisTels();
+					//System.out.println(persona.getVards()+" "+persona.getKurss());
+					String m = GalvenaisTels.virknesParbaude("Mašīnas nosaukumu ievade", "Bravado Buffalo");
+					if(m == null)
+						break;
+					persona.setMasina(m);
+					String u = GalvenaisTels.virknesParbaude("Studentu uzvārdu ievade", "Epstīns");
+					if(u == null)
+						break;
+						ieroci = Integer.parseInt(JOptionPane.showInputDialog("Ievadi ieroču skaitu", 1));
+						persona.setIeroci(ieroci);
+						
+							personas.add(persona);	
+					break;
+				break;
+				}
+			case 1:
+				if(personas.size()>0) {
+					int ritID = GalvenaisTels.personasIzvele(persona);
+					personas.remove(ritID);
+					JOptionPane.showMessageDialog(null, "Ritenis ir veiksmīgi dzēsts!", "Brīdinājums", JOptionPane.WARNING_MESSAGE);
+				}else {
+					JOptionPane.showMessageDialog(null, "Sarakstā nav neviens ritenis!", "Brīdinājums", JOptionPane.WARNING_MESSAGE);
+				}
+				break;
+			case 5:
+				JOptionPane.showMessageDialog(null, "Programma apturēta!", "Brīdinājums", JOptionPane.WARNING_MESSAGE);
+				break;
+			}
+		}while(izvelesID != 5);
+	}
+	}
+
+}
